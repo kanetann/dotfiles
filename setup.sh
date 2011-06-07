add952b55bff11466d920f187d3ad20d5c426b5e
kanetann <kanetann@gmail.com>
2011-06-07 14:57:28 +0900

    add .bashrc

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zsh ~/.zsh
 ln -s ~/dotfiles/.screenrc ~/.screenrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
 
 git clone git://github.com/gmarik/vundle.git ~/dotfiles/.vim/bundle/vundle
 
