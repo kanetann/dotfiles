176153c7359d5a28859d37b94415633cd46afd41
kanetann <kanetann@gmail.com>
2012-01-12 17:34:04 +0900

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # add symlinks 
-ln -s ~/dotfiles/.vimperatorrc ~/.vimperatorrc
+#ln -s ~/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zsh ~/.zsh
-ln -s ~/dotfiles/.screenrc ~/.screenrc
+#ln -s ~/dotfiles/.screenrc ~/.screenrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+#ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.ssh/config ~/.ssh/config
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.proverc ~/.proverc
@@ -19,21 +19,30 @@ ln -s ~/dotfiles/.proverc ~/.proverc
 git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
 git config --global http.sslVerify false
 
-mkdir ~/dotfiles/.vim/dict
-cd ~/dotfiles/.vim/dict
-wget http://www.php.net/get/php_manual_ja.tar.gz/from/jp2.php.net/mirror
-tar zxvf php_manual_ja.tar.gz
+#mkdir ~/dotfiles/.vim/dict
+#cd ~/dotfiles/.vim/dict
+#wget http://www.php.net/get/php_manual_ja.tar.gz/from/jp2.php.net/mirror
+#tar zxvf php_manual_ja.tar.gz
 
 # install vvm(vim version manager like rvm.)
-curl https://raw.github.com/kana/vim-version-manager/master/bin/vvm | python - setup
-cat << EOS >> $HOME/.zshrc 2>&1
-test -f ~/.vvm/etc/login && source ~/.vvm/etc/login
-EOS
-vvm install vimorg--v7-3-254
-vvm use vimorg--v7-3-254
+#curl https://raw.github.com/kana/vim-version-manager/master/bin/vvm | python - setup
+#cat << EOS >> $HOME/.zshrc 2>&1
+#test -f ~/.vvm/etc/login && source ~/.vvm/etc/login
+#EOS
+#vvm install vimorg--v7-3-254
+#vvm use vimorg--v7-3-254
 
 # install oh-my-zsh
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
 # install edit-server for chrome
-git clone https://github.com/gfxmonk/edit-server.git
+#git clone https://github.com/gfxmonk/edit-server.git
+
+# please see to below url. 
+# Bundle 'git://github.com/Shougo/vimproc write to .vimrc.
+# open vim and try :BundleInstall.
+cd .vim/bundle/vimproc
+
+# if you using macosx, make -f make_mac.mak
+make -f make_gcc.mak
+
