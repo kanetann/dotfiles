7be5ee545507a2e02d730ecc27414c6b1bff793a
kanetann <kanetann@gmail.com>
2013-03-28 16:26:23 +0900

    moved setup*.sh

--- vvminstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl https://raw.github.com/kana/vim-version-manager/master/bin/vvm | python - setup
-cat << EOS >> $HOME/.zshrc 2>&1
-test -f ~/.vvm/etc/login && source ~/.vvm/etc/login
-EOS
-vvm install vimorg--v7-3-254
-vvm use vimorg--v7-3-254
