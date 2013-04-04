ba0cd88180e8a15d6ea4ce72e3ccded33fb5b23a
kanetann <kanetann@gmail.com>
2013-04-04 11:44:39 +0900

    updated setup.rb

--- setup/setup.rb
+++ setup/setup.rb
@@ -1,47 +1,54 @@
 #!/usr/bin/env ruby
 
-# Command Line Toolsの手動インストール
-# https://developer.apple.com/downloads/index.action
 
-# install homebrew
-p "ruby -e \"$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)\""
+def execute(cmd)
+  p `#{cmd}`
+end
+
+uname = `uname`
+
+if uname == "Darwin" then 
+  # Command Line Toolsの手動インストール https://developer.apple.com/downloads/index.action
+  # install homebrew
+  execute "ruby -e \"$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)\""
+end
 
 # install brew tools
-tools = %w[rbenv ruby-build tree nkf wget v8 vim zsh git-core make gcc ctags curl hub tig tmux reattach-to-user-namespace the_silver_searcher]
+tools = %w{rbenv ruby-build tree nkf wget v8 vim zsh git make gcc ctags curl tig tmux reattach-to-user-namespace the_silver_searcher}
 
-tools.each do |t|
+tools.each do |k|
   if uname == "Darwin" then 
-    if t == "git-core" then
-      t = git
-    end
-    p "brew install #{t}"
+    k = "git-core" if k == "git"
+    execute "brew install #{k}"
   else
-    p "sudo aptitude -y install #{t}"
+    next if k == "reattach-to-user-namespace"
+    next if k == "v8"
+    execute "sudo aptitude -y install #{k}"
   end
 end
 
 # git config
-p "git config --global http.sslVerify false"
+execute "git config --global http.sslVerify false"
 
 # add symlinks
-dotfiles = %w[.vimrc .vim .zshrc .zsh .bashrc .bash_profile .ssh/config .gitconfig .proverc .gemrc .inputrc .pryrc]
-dotfiles.each do |d|
-  p "rm -rf ~/#{t}"
-  p "ln -s ~/dotfiles/#{t} ~/#{t}"
+dotfiles = %w{.vimrc .vim .zshrc .zsh .bashrc .bash_profile .ssh/config .gitconfig .proverc .gemrc .inputrc .pryrc}
+dotfiles.each do |k|
+  execute "rm -rf ~/#{k}"
+  execute "ln -s ~/dotfiles/#{k} ~/#{k}"
 end
 
 # gems
-gems = %w[rbenv-rehash rails vagrant virtualbox vagrant-snap awesome_print pry pry-doc refe2 heroku]
-dotfiles.each do |d|
-  p "gem install #{t}"
+gems = %w{rbenv-rehash rails vagrant virtualbox vagrant-snap awesome_print pry pry-doc refe2 heroku}
+dotfiles.each do |k|
+  execute "gem install #{k}"
 end
 
 # install oh-my-zsh
-p "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
+execute "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
 
 # setup NeoBundle
-p "git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim"
-p "vim -N -u NONE -i NONE -V1 -e -s --cmd \"source ~/.vimrc\" --cmd NeoBundleInstall! --cmd qall!"
+execute "git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim"
+execute "vim -N -u NONE -i NONE -V1 -e -s --cmd \"source ~/.vimrc\" --cmd NeoBundleInstall! --cmd qall!"
 
 # setup vimproc
 if uname == "Darwin" then 
@@ -49,4 +56,5 @@ if uname == "Darwin" then
 else
   procfilename = "make_unix.mak"
 end
-p "cd ~/.vim/bundle/vimproc && make -f #{procfilename}"
+execute "cd ~/.vim/bundle/vimproc && make -f #{procfilename}"
+
