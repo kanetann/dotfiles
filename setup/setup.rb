8f0cc95190060282871b291b4e8a897c5f5c3b8c
kanetann <kanetann@gmail.com>
2014-04-24 15:35:44 +0900

    update.

--- setup/setup.rb
+++ setup/setup.rb
@@ -4,61 +4,92 @@
 # wget --no-check-certificate -O - https://raw.github.com/kanetann/dotfiles/master/setup/setup.rb | ruby
 
 def execute(cmd)
-  p `#{cmd}`
+    p `#{cmd}`
 end
 
 uname = `uname`.chomp
 
 # tools install
 if uname == "Darwin" then
-  # Command Line Toolsの手動インストール https://developer.apple.com/downloads/index.action
-  # install homebrew
-  # execute "ruby -e \"$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)\""
-  tools = %w{ruby-build nkf autoconf the_silver_searcher reattach-to-user-namespace tig tmux ctags wget}
-  tools.each do |k|
-    execute "brew install #{k}"
-  end
+    tools = [
+        'autoconf',
+        'ctags',
+        'nkf',
+        'reattach-to-user-namespace',
+        'ruby-build',
+        'the_silver_searcher',
+        'tig',
+        'tmux',
+        'wget',
+      ]
+      tools.each do |k|
+            execute "brew install #{k}"
+              end
 else
-  execute "sudo apt-get update"
-  tools = %w{tree nkf wget vim zsh git-core ctags curl tig tmux the_silver_searcher make gcc}
-  tools.each do |k|
-    execute "sudo aptitude -y install #{k}"
-  end
+    execute "sudo apt-get update"
+      tools = [
+          'ctags',
+          'curl',
+          'gcc',
+          'git-core',
+          'make',
+          'nkf',
+          'the_silver_searcher',
+          'tig',
+          'tmux',
+          'tree',
+          'vim',
+          'wget',
+          'zsh',
+        ]
+        tools.each do |k|
+              execute "sudo aptitude -y install #{k}"
+                end
 end
 
 # git config
 execute "git config --global http.sslVerify false"
-
-# git clone
 execute "git clone git://github.com/kanetann/dotfiles.git ~/dotfiles"
 
-# delete symlinks
-dotfiles = %w{.bash_profile .bashrc .gemrc .gitconfig .gitignore .inputrc .proverc .pryrc .screenrc .tmux.conf .vimperatorrc .vimrc .vim .zsh .zshenv .zshrc .ssh/config .tmux.conf .agignore .ctags}
+# update symlinks
+dotfiles = [
+  '.agignore',
+  '.bash_profile',
+  '.bashrc',
+  '.ctags',
+  '.gemrc',
+  '.gitconfig',
+  '.gitignore',
+  '.inputrc',
+  '.proverc',
+  '.pryrc',
+  '.screenrc',
+  '.tmux.conf',
+  '.vim',
+  '.vimperatorrc',
+  '.vimrc',
+  '.zsh',
+  '.zshenv',
+  '.zshrc',
+]
 dotfiles.each do |k|
-  execute "rm -rf ~/#{k}"
+    execute "rm -rf ~/#{k}"
+      execute "ln -s ~/dotfiles/#{k} ~/#{k}"
 end
 
 # setup NeoBundle
-execute "ln -s ~/dotfiles/.vimrc ~/.vimrc"
-execute "ln -s ~/dotfiles/.vim ~/.vim"
 execute "git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim"
 execute "vim -N -u NONE -i NONE -V1 -e -s --cmd \"source ~/.vimrc\" --cmd NeoBundleInstall! --cmd qall!"
 
 # setup vimproc
 if uname == "Darwin" then
-  procfilename = "make_mac.mak"
+    procfilename = "make_mac.mak"
 else
-  procfilename = "make_unix.mak"
+    procfilename = "make_unix.mak"
 end
 execute "cd ~/.vim/bundle/vimproc && make -f #{procfilename}"
 
 # install oh-my-zsh
 execute "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
-
-# add symlinks
-dotfiles.each do |k|
-  execute "ln -s ~/dotfiles/#{k} ~/#{k}"
-end
-
 execute "rm -rf ~/.zshrc"
 execute "ln -s ~/dotfiles/.zshrc ~/.zshrc"
